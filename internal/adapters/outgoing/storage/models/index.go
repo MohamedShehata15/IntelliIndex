800a96669330b70fa34f7934e026b371c64102dd
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-01 08:41:20 +0300

    add BeforeCreate hook to Index model to generate UUID if ID is empty

--- internal/adapters/outgoing/storage/models/index.go
+++ internal/adapters/outgoing/storage/models/index.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 // Index represents the database model for a search index
 type Index struct {
 	BaseModel
@@ -8,4 +13,12 @@ type Index struct {
 	SettingsJSON string     `gorm:"type:text;column:settings"`
 	MappingsJSON string     `gorm:"type:text;column:mappings"`
 	Documents    []Document `gorm:"foreignKey:IndexID"`
+}
+
+// BeforeCreate is a GORM hook that generates a UUID if ID is empty
+func (i *Index) BeforeCreate(tx *gorm.DB) (err error) {
+	if i.ID == "" {
+		i.ID = uuid.NewString()
+	}
+	return
 }
