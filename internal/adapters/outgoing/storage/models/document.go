d892b9fd1e8a1007b18ecce0600de067678430e6
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-01 22:46:41 +0300

    add BeforeCreate hook and ToDomain conversion method to Document model

--- internal/adapters/outgoing/storage/models/document.go
+++ internal/adapters/outgoing/storage/models/document.go
@@ -1,6 +1,13 @@
 package models
 
-import "time"
+import (
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/mohamedshehata15/intelli-index/internal/core/domain"
+)
 
 // Document represents the database model for a document
 type Document struct {
@@ -20,4 +27,44 @@ type Document struct {
 	DocumentMetadata DocumentMetadata  `gorm:"foreignKey:DocumentID"`
 	DocumentLinks    []DocumentLink    `gorm:"foreignKey:SourceID"`
 	DocumentKeywords []DocumentKeyword `gorm:"foreignKey:DocumentID"`
+}
+
+// BeforeCreate is a GORM hook that generates a UUID if ID is empty
+func (d *Document) BeforeCreate() (err error) {
+	if d.ID == "" {
+		d.ID = uuid.NewString()
+	}
+	return
+}
+
+// ToDomain converts the database model to a domain entity
+func (d *Document) ToDomain() *domain.Document {
+	doc := &domain.Document{
+		ID:             d.ID,
+		URL:            d.URL,
+		Title:          d.Title,
+		Content:        d.Content,
+		ContentType:    domain.ContentType(d.ContentType),
+		LastCrawled:    d.LastCrawled,
+		LastModified:   d.LastModified,
+		Lang:           d.Lang,
+		MetaDesc:       d.MetaDesc,
+		ContentLength:  d.ContentLength,
+		ImportanceRank: d.ImportanceRank,
+		IndexID:        d.IndexID,
+		StatusCode:     http.StatusOK,
+		MetaKeywords:   make([]string, 0),
+		Links:          make([]string, 0),
+		ParsedContent:  make(map[string]interface{}),
+	}
+
+	for _, keyword := range d.DocumentKeywords {
+		doc.MetaKeywords = append(doc.MetaKeywords, keyword.Keyword)
+	}
+
+	for _, link := range d.DocumentLinks {
+		doc.Links = append(doc.Links, link.TargetURL)
+	}
+
+	return doc
 }
