daa7687456307f2cc21c0f6907e7d7323067287f
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-05 19:56:22 +0300

    implement base IndexRepository interface Method

--- internal/adapters/outgoing/elasticsearch/index_repository.go
+++ internal/adapters/outgoing/elasticsearch/index_repository.go
@@ -1,8 +1,12 @@
 package elasticsearch
 
 import (
+	"context"
 	"sync"
 	"time"
+
+	"github.com/mohamedshehata15/intelli-index/internal/core/domain"
+	"github.com/mohamedshehata15/intelli-index/internal/core/ports/outgoing"
 )
 
 // IndexRepository implements the outgoing.IndexRepository interface
@@ -10,6 +14,54 @@ type IndexRepository struct {
 	client         *Client
 	refreshTickers map[string]*time.Ticker
 	refreshMutex   sync.RWMutex
+}
+
+// Ensure IndexRepository implements the outgoing.IndexRepository interface
+var _ outgoing.IndexRepository = (*IndexRepository)(nil)
+
+func (i IndexRepository) Create(ctx context.Context, index *domain.Index) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) GetByID(ctx context.Context, id string) (*domain.Index, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) GetByName(ctx context.Context, name string) (*domain.Index, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) List(ctx context.Context) ([]*domain.Index, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) Delete(ctx context.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) Update(ctx context.Context, index *domain.Index) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) UpdateSettings(ctx context.Context, id string, settings domain.IndexSettings) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) GetStats(ctx context.Context, id string) (map[string]interface{}, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i IndexRepository) RefreshIndex(ctx context.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
 }
 
 // NewIndexRepository creates a new Elasticsearch index repository
