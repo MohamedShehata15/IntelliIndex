44c0e9e39de0958c2ab6a0c6ce17e7a2947f97aa
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-09 18:01:23 +0300

    add ParseResponse helper function for parsing Elasticsearch responses into specified structures

--- internal/adapters/outgoing/elasticsearch/utils.go
+++ internal/adapters/outgoing/elasticsearch/utils.go
@@ -3,6 +3,7 @@ package elasticsearch
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 )
 
 // mustMarshalJSON marshals data to JSON and panics if there's an error
@@ -12,4 +13,20 @@ func mustMarshalJSON(v interface{}) []byte {
 		panic(fmt.Sprintf("failed to marshal JSON: %v", err))
 	}
 	return data
+}
+
+// ParseResponse is a generic helper function that parses an Elasticsearch response body into a given interface
+func ParseResponse(body io.ReadCloser, result interface{}) error {
+	defer func(Body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+			fmt.Printf("error closing response body: %v\n", err)
+		}
+	}(body)
+
+	if err := json.NewDecoder(body).Decode(result); err != nil {
+		return fmt.Errorf("error parsing response: %w", err)
+	}
+
+	return nil
 }
