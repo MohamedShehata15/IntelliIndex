29ceca492ad480137c6ebb84448133a8b8065ecf
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-17 18:59:12 +0300

    add WebCrawlerAdapterFactory for configuring web crawler adapters

---
+++ internal/adapters/outgoing/webcrawler/adapter.go
@@ -0,0 +1,33 @@
+package webcrawler
+
+import (
+	"github.com/mohamedshehata15/intelli-index/internal/pkg/di"
+	"github.com/mohamedshehata15/intelli-index/pkg/config"
+)
+
+// WebCrawlerAdapterFactory encapsulates the configuration and registration logic
+type WebCrawlerAdapterFactory struct {
+	config *config.CrawlerConfig
+}
+
+var _ di.AdapterRegistrar = (*WebCrawlerAdapterFactory)(nil)
+
+func NewWebCrawlerAdapterFactory(cfg *config.CrawlerConfig) *WebCrawlerAdapterFactory {
+	return &WebCrawlerAdapterFactory{
+		config: cfg,
+	}
+}
+
+// Register implements the AdapterRegistrar interface
+func (w *WebCrawlerAdapterFactory) Register(container *di.Container) error {
+	return RegisterWebCrawlerAdapter(container, w.config)
+}
+
+// RegisterWebCrawlerAdapter registers the web crawler adapter with the dependency injection container
+func RegisterWebCrawlerAdapter(container *di.Container, cfg *config.CrawlerConfig) error {
+	// Register web crawler implementation
+	container.Register("webCrawler", func() (interface{}, error) {
+		return NewWebCrawler(cfg), nil
+	})
+	return nil
+}
