2cf20de45b9f0f927258cac82d7be4ed15af320b
Mohamed Shehata <mohamed159716@yahoo.com>
2025-06-17 18:53:12 +0300

    add Crawler implementation skeleton with unimplemented methods

---
+++ internal/adapters/outgoing/webcrawler/crawler.go
@@ -0,0 +1,47 @@
+package webcrawler
+
+import (
+	"context"
+	"github.com/mohamedshehata15/intelli-index/internal/core/domain"
+	"github.com/mohamedshehata15/intelli-index/internal/core/ports/outgoing"
+)
+
+// Crawler implements the outgoing.WebCrawler interface
+type Crawler struct{}
+
+var _ outgoing.WebCrawler = (*Crawler)(nil)
+
+// NewWebCrawler creates a new instance of Crawler
+func NewWebCrawler() *Crawler {
+	return &Crawler{}
+}
+
+func (c *Crawler) Crawl(ctx context.Context, url string) (*domain.CrawlResult, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Crawler) StartCrawlingJob(ctx context.Context, seedURLs []string, options *domain.CrawlOptions) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Crawler) StopCrawlingJob(ctx context.Context, jobID string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Crawler) GetCrawlingJobStatus(ctx context.Context, jobID string) (*domain.CrawlProgress, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Crawler) AddURLToCrawl(ctx context.Context, jobID, url string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Crawler) SetCrawlResultHandler(handler func(ctx context.Context, result *domain.CrawlResult) error) {
+	//TODO implement me
+	panic("implement me")
+}
